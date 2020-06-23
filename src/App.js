4ec4be097f6e59b440bd093f642b02e35e7af333
JealousGx <59867217+JealousGx@users.noreply.github.com>
2020-06-23 19:45:58 +0500

    Basic React-App Tutorial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App(data) {
+  return (
+          <div>
+            Hello from App.js <br/>{data.name}
+          </div>
+  );
+}
+
+export default App;
